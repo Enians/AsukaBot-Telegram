93609d3eab308dc1357d0bedaa918c3f03372347
Hernan Saavedra Radovcic <nanoskull@gmail.com>
2020-08-01 20:15:34 -0400

    Refinamiento Codigo

--- functions/commons.js
+++ functions/commons.js
@@ -1,60 +1,34 @@
 
 const moment = require('moment-timezone');
+const gifs = require('./gifContainer');
+const msgs = require('./mensajes');
 module.exports = {
     esJueves: function () {
-        console.log(moment().tz("America/Santiago").locale('es-CL').format('dddd'));
         return moment().tz("America/Santiago").locale('es-CL').format('dddd') == 'jueves';
     },
 
-    getRandomGif: function() {
-        
-        min = Math.ceil(1);
-        max = Math.floor(5);
-        var rng = Math.floor(Math.random() * (max - min + 1)) + min;
-
-        const AnimationUrl1 = 'https://data.whicdn.com/images/149722992/original.gif';
-        const AnimationUrl2 = 'https://lh3.googleusercontent.com/proxy/Dzeds73w5bznOXOqmJtztceJA9iV5M8esWlAafJ0jNrrjXyrgPYE3CbH54KU83AYs8z1SZu5xibpixPCbpmXllgV0UoIYO6zEiSDWuhbpMe6';
-        const AnimationUrl3 = 'https://thumbs.gfycat.com/OddSecondaryArchaeopteryx-small.gif';
-        const AnimationUrl4 = 'https://64.media.tumblr.com/tumblr_lgu7rlghZV1qfwdhao1_500.gif'
-        const AnimationUrl5 = 'https://phoneky.co.uk/thumbs/screensavers/down/new/anime/asuka7575_Ql8RhPIV.gif';
-
-        switch(rng){
-            case 1:
-                return AnimationUrl1;
-            case 2:
-                return AnimationUrl2;
-            case 3:
-                return AnimationUrl3;
-            case 4:
-                return AnimationUrl4;
-            case 5:
-                return AnimationUrl5;
-        }
+    queDia: function(){
+        return moment().tz("America/Santiago").locale('es-CL').format('dddd');
     },
 
-    getBakaGif: function(){
-        min = Math.ceil(1);
-        max = Math.floor(5);
-        var rng = Math.floor(Math.random() * (max - min + 1)) + min;
+    getJuevesGif: function() {
+        var gif = new gifs.siJuevesgIF();
+        return gif.GetGif();
+    },
 
-        const AnimationUrl1 = 'https://i.pinimg.com/originals/09/8e/d5/098ed5b62803063a74eb519ee415c7e6.gif';
-        const AnimationUrl2 = 'https://i.imgur.com/trjJKuw.gif';
-        const AnimationUrl3 = 'https://thumbs.gfycat.com/UnlawfulAmbitiousAlabamamapturtle-size_restricted.gif';
-        const AnimationUrl4 = 'https://gifimage.net/wp-content/uploads/2017/07/asuka-gif-4.gif'
-        const AnimationUrl5 = 'https://i.gifer.com/C1Jc.gif';
+    getNoJuevesGif: function(){
+        var gif = new gifs.noJuevesGif();
+        return gif.GetGif();
+    },
 
-        switch(rng){
-            case 1:
-                return AnimationUrl1;
-            case 2:
-                return AnimationUrl2;
-            case 3:
-                return AnimationUrl3;
-            case 4:
-                return AnimationUrl4;
-            case 5:
-                return AnimationUrl5;
-        }
+    getJuevesMsg: function(){
+        var msg = new msgs.siEsJueves();
+        return msg.GetMessage();
+    },
+
+    getNoJuevesMsg: function(p_dia){
+        var msg = new msgs.noEsJueves(p_dia);
+        return msg.GetMessage();
     },
 
     getToucan: function(){
